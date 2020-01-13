7c20ef9131745d00b5e21b9bbf879c6ef0a9eca6
Gary <gvelasquez@beemining.cl>
2020-01-13 15:53:59 -0300

    modified mutat route
    changes frol old local repository

--- src/routes/mutantController.js
+++ src/routes/mutantController.js
@@ -7,7 +7,27 @@ const dnaReader = require('../utils/dnaReader');
 router.use(bodyParser.json());
 
 router.post('/', async function(req, res) {
-  
+  try {
+    //validate actual input
+    dnaReader.validateInput(req.body['dna']);
+  } catch (e) {
+    return res.status(e.code).send(e.message);
+  };
+  //check if actual DNA record is already stored in database
+  const result = await database.queryDNA(req.body['dna']);
+  if (result != null && result.hasOwnProperty('dna_value') && result.hasOwnProperty('mutant')) {
+    //DNA record found nothing more to do
+    res.status(200).send(result['mutant']);
+  } else {
+    //store new DNA record after analysis
+    const mutantFlag = dnaReader.isMutant(req.body['dna']);
+    await database.insertDNA(req.body['dna'], mutantFlag);
+    if (mutantFlag) {
+      res.status(200).send(mutantFlag);
+    } else {
+      res.status(403).send(mutantFlag);
+    }
+  }
 });
 
 module.exports = router;
