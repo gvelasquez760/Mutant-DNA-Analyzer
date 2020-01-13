2a277ffcf8ee6f9506eb2d4c59f178f598925177
Gary <gvelasquez@beemining.cl>
2020-01-13 15:56:04 -0300

    modified sequelize index file

---
+++ src/database/index.js
@@ -0,0 +1,59 @@
+const Sequelize = require('sequelize');
+const config = require(__dirname + '/config/config.js')[process.env.NODE_ENV];
+const sequelize = new Sequelize(config);
+const dna_landing = sequelize.import ('../models/dna_landing');
+const op = Sequelize.Op;
+
+module.exports = {
+  truncateLanding: async () => {
+    await dna_landing.sync({force: true});
+    console.log('dna_landing truncated');
+  },
+  checkConnection: async () => {
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+    } catch (err) {
+      console.error('Unable to connect to the database:', err);
+      return err;
+    }
+  },
+  queryDNA: async (dna) => {
+    try {
+      const result = await dna_landing.findOne({
+        where: {
+          dna_value: {
+            [op.eq]: dna
+          }
+        }
+      });
+      return (
+        result != null
+        ? result['dataValues']
+        : result);
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  },
+  insertDNA: async (dnaValue, mutantFlag) => {
+    try {
+      await dna_landing.create({dna_value: dnaValue, mutant: mutantFlag});
+      console.log('dna saved');
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  },
+  getMutantRatio: async () => {
+    try {
+      return sequelize.query(`SELECT COUNT(*) filter (where "mutant")
+       as count_mutant_dna, COUNT( *)filter(where not "mutant")as count_human_dna,
+       CASE WHEN count( *)filter(where not "mutant") = 0 THEN 0 ELSE(TRUNC(COUNT( *)filter(where "mutant")::numeric / COUNT( *)filter(where not "mutant"),
+        1))END as ratio from public.dna_landing;`, {type: sequelize.QueryTypes.SELECT});
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+  }
+}
